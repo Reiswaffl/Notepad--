1f15528ab163e54727c2c9e40b9432c34f3a79cd
Reiswaffl <38401280+Reiswaffl@users.noreply.github.com>
2018-11-07 20:00:22 +0100

    Converter addNote

--- src/main/java/com/Notepad/XML/Converter.java
+++ src/main/java/com/Notepad/XML/Converter.java
@@ -2,31 +2,39 @@ package main.java.com.Notepad.XML;
 
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
-
 import org.w3c.dom.Node;
 
 public class Converter {
     private XMLReader xmlReader;
 
-    public void Converter(){
+    public Converter(){
         setup();
     }
 
     public void setup(){
         xmlReader = new XMLReader("resources/saves.xml");
     }
+
+    /**
+     * @brief allows to add a note
+     * @param user user, the note is from
+     * @param date date, the appointment is at
+     * @param start start, the note has
+     * @param end end, the note has
+     * @param content content, the note has
+     */
     public void addNote(String user, String date, String start, String end, String content){
         NodeList users = xmlReader.getNodes("user");
         if(users != null) {
-            for (int i = 0; i < user.length(); i++) {
+            for (int i = 0; i < users.getLength(); i++) {
                 Node userNode = users.item(i);
                 if (userNode.getNodeType() == Node.ELEMENT_NODE) {
                     Element element = (Element) userNode;
                     if (element.getAttribute("id").equals(user)) {
                         Node note = xmlReader.setNode(userNode, "note");
                         xmlReader.setAtt(note, "date", date);
-                        xmlReader.setAtt(note, "start", start);
-                        xmlReader.setAtt(note, "end", end);
+                        if(start != null) xmlReader.setAtt(note, "start", start);
+                        if(end != null) xmlReader.setAtt(note, "end", end);
                         xmlReader.setContent(note, content);
                     }
                 }
