9edde4efafbe66c1c60bc7f678a9548424c12b32
Reiswaffl <38401280+Reiswaffl@users.noreply.github.com>
2018-11-14 18:28:04 +0100

    GUI

--- src/main/java/com/Notepad/View/MainSceneController.java
+++ src/main/java/com/Notepad/View/MainSceneController.java
@@ -35,4 +35,98 @@ public class MainSceneController implements Initializable{
      */
     public void changeUser(ActionEvent event) {
     }
+
+    public void handleI1(ActionEvent event) {
+    }
+
+    public void handleI2(ActionEvent event) {
+    }
+
+    public void handleI3(ActionEvent event) {
+    }
+
+    public void handleI4(ActionEvent event) {
+    }
+
+    public void handleI5(ActionEvent event) {
+    }
+
+    public void handleI6(ActionEvent event) {
+    }
+
+    public void handleI7(ActionEvent event) {
+    }
+
+    public void handleI8(ActionEvent event) {
+    }
+
+    public void handleI9(ActionEvent event) {
+    }
+
+    public void handleI10(ActionEvent event) {
+    }
+
+    public void handleI11(ActionEvent event) {
+    }
+
+    public void handleI12(ActionEvent event) {
+    }
+
+    public void handleI13(ActionEvent event) {
+    }
+
+    public void handleI14(ActionEvent event) {
+    }
+    
+    public void handleI15(ActionEvent event) {
+    }
+    
+    public void handleI16(ActionEvent event) {
+    }
+
+
+    public void handleI17(ActionEvent event) {
+    }
+
+    public void handleI18(ActionEvent event) {
+    }
+
+    public void handleI19(ActionEvent event) {
+    }
+
+    public void handleI20(ActionEvent event) {
+    }
+
+    public void handleI21(ActionEvent event) {
+    }
+
+    public void handleI22(ActionEvent event) {
+    }
+
+    public void handleI23(ActionEvent event) {
+    }
+
+    public void handleI24(ActionEvent event) {
+    }
+
+    public void handleI25(ActionEvent event) {
+    }
+
+    public void handleI26(ActionEvent event) {
+    }
+
+    public void handleI27(ActionEvent event) {
+    }
+
+    public void handleI28(ActionEvent event) {
+    }
+
+    public void handleI29(ActionEvent event) {
+    }
+
+    public void handleI30(ActionEvent event) {
+    }
+
+    public void handleI31(ActionEvent event) {
+    }
 }
